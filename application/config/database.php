f38d95aa3c4b31cd91e5348433fb759805badba3
rifqi afif <rifqi.afif@lumoshive.com>
2022-11-28 11:26:04 +0800

    setup db

--- application/config/database.php
+++ application/config/database.php
@@ -6,10 +6,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'db_simkopsis',
+	'hostname' => 'pusjak-llatp.com',
+	'username' => 'u540115449_admin',
+	'password' => 'Pusjak@2022',
+	'database' => 'db_pusjak',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
