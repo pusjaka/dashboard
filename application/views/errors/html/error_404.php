129a7f1ccd96e947ed2997122feed3e2c75009d5
rifqi afif <rifqi.afif@lumoshive.com>
2022-12-22 09:57:58 +0800

    update 404 page

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -76,9 +76,7 @@
                             </div>
                             <div id="site-layout-example-right" class="col s12 m12 l12">
                                 <div class="row center">
-                                    <h1 class="text-long-shadow col s12">404</h1>
-                                </div>
-                                <div class="row center">
+                                    <h1 class="text-long-shadow col s12">404</h1><br>
                                     <p class="center white-text col s12">It seems that this page doesn’t exist.</p>
                                     <p class="center s12"><button onclick="goBack()" class="btn waves-effect waves-light">Back</button> <a href="index.html" class="btn waves-effect waves-light">Homepage</a>
                                     <p>
