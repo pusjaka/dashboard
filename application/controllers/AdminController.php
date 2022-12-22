dd7de4c787304ae8afe16be25274fed9f7bbff81
rifqi afif <rifqi.afif@lumoshive.com>
2022-12-22 10:11:36 +0800

    set restriction user level to access admin feature

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -8,6 +8,8 @@
 			if (!parent::hasLogin()) {
 				$this->session->set_flashdata('alert', 'belum_login');
 				redirect(base_url('login'));
+			}else if(parent::hasLogin() && $this->session->userdata['level'] == 'user'){
+				redirect(base_url('dashboard'));
 			}
 		}
 		
